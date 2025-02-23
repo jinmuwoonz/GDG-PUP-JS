a9cf240e1da9dbfc3ba24380b1a5eb12ff0773dc
jinmuwoonz <ategadaniel@gmail.com>
2025-02-23 11:30:40 +0800

    Update script.js

---
+++ Assignment-Exercises/JS-5_Arrays-and-Loops/script.js
@@ -0,0 +1,13 @@
+// there's no content/instruction in the file
+
+const favoriteMovies = [
+  "Jack The Giant Slayer",
+  "How To Train Your Dragon",
+  "World War Z",
+  "Warm Bodies",
+  "John Wick",
+];
+
+for (let i = 0; i < favoriteMovies.length; i++) {
+  console.log(favoriteMovies[i]);
+}
