6eb8c70caa79427cab03647681901c0aef536292
jinmuwoonz <ategadaniel@gmail.com>
2025-02-25 12:19:15 +0800

    Update JS8.js

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,26 @@
-// your code starts here
+const counterText = document.getElementById("counter-text");
+const subtractBtn = document.querySelector("#button-subtract");
+const addBtn = document.querySelector("#button-add");
+
+let counterValue = parseInt(counterText.textContent, 10); // this converts the string into integer with base 10
+
+subtractBtn.addEventListener("click", subtractValue);
+addBtn.addEventListener("click", addValue);
+
+function subtractValue() {
+  if (counterValue !== 0) {
+    counterValue--;
+    updateTextContent();
+  } else {
+    return;
+  }
+}
+
+function addValue() {
+  counterValue++;
+  updateTextContent();
+}
+
+function updateTextContent() {
+  counterText.textContent = counterValue;
+}
